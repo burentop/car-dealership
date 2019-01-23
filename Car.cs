4937422c6c72c5e6a7add5f2d609144f1d61a616
Brent Perry <contact@brentallan.me>
2019-01-22 18:50:26 -0600

    add 4 Cars

--- Car.cs
+++ Car.cs
@@ -7,3 +7,31 @@ class Car
     public int Price;
     public int Miles;
 }
+
+public class Program
+{
+    public static void Main()
+    {
+        Car porsche = new Car();
+        porsche.MakeModel = "2014 Porsche 911";
+        porsche.Price = 114991;
+        porsche.Miles = 7864;
+
+        Car ford = new Car();
+        ford.MakeModel = "2011 Ford F450";
+        ford.Price = 55995;
+        ford.Miles = 14241;
+
+        Car lexus = new Car();
+        lexus.MakeModel = "2013 Lexus RX 350";
+        lexus.Price = 44700;
+        lexus.Miles = 20000;
+
+        Car mercedes = new Car();
+        mercedes.MakeModel = "Mercedes Benz CLS550";
+        mercedes.Price = 39900;
+        mercedes.Miles = 37979;
+
+        List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
+    }
+}
