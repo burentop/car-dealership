2df3a23707b9a1762f5a77487664dd4c63881c67
Brent Perry <contact@brentallan.me>
2019-01-23 18:22:33 -0600

    add Program.cs file and separate logic

--- Car.cs
+++ Car.cs
@@ -1,53 +1,22 @@
 using System;
-using System.Collections.Generic;
 
-class Car
-{
-    public string MakeModel;
-    public int Price;
-    public int Miles;
-
-    public Car(string makeModel, int price, int miles)
+namespace Dealership {
+    class Car
     {
-        MakeModel = makeModel;
-        Price = price;
-        Miles = miles;
-    }
+        public string MakeModel;
+        public int Price;
+        public int Miles;
 
-    public bool WorthBuying(int maxPrice)
-    {
-        return (Price < maxPrice);
-    }
-}
-
-public class Program
-{
-    public static void Main()
-    {
-        Car porsche = new Car("2014 Porsche 911", 114991, 7864);
-        Car ford = new Car("2011 Ford F450", 55995, 14241);
-        Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
-        Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
-        
-        List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
-
-        Console.WriteLine("Enter maximum price: ");
-        string stringMaxPrice = Console.ReadLine();
-        int maxPrice = int.Parse(stringMaxPrice);
-
-        List<Car> CarsMatchingSearch = new List<Car>();
-
-        foreach (Car automobile in Cars)
+        public Car(string makeModel, int price, int miles)
         {
-            if (automobile.WorthBuying(maxPrice))
-            {
-                CarsMatchingSearch.Add(automobile);
-            }
+            MakeModel = makeModel;
+            Price = price;
+            Miles = miles;
         }
 
-        foreach (Car automobile in CarsMatchingSearch)
+        public bool WorthBuying(int maxPrice)
         {
-            Console.WriteLine(automobile.MakeModel);
+            return (Price < maxPrice);
         }
     }
 }
